08ec0b4e7e006cf15e54fe277eacf52bcd3f1b4d
JasperMunene <jaspermunene2022@gmail.com>
2024-11-08 00:48:58 +0300

    Complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [style, setStyle] = useState("")
+
+  const handleAdd = () => {
+    setStyle("in-cart")
+  }
   return (
-    <li className="">
+    <li className={style}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAdd}>Add to Cart</button>
     </li>
   );
 }
